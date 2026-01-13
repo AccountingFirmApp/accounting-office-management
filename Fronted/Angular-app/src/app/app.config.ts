91ad9e39f771815d60df9229ba323473fe985f16
malka3949 <ms0534113949@gmail.com>
2026-01-13 10:09:46 +0200

    crud company and tasks

--- Fronted/Angular-app/src/app/app.config.ts
+++ Fronted/Angular-app/src/app/app.config.ts
@@ -1,19 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient,withInterceptors  } from '@angular/common/http';  // ⬅️ הוסף את זה
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideHttpClient() , // ⬅️ הוסף את זה
-    provideAnimations(),
-    importProvidersFrom(FormsModule)
-
+    provideRouter(routes)
   ]
 };
