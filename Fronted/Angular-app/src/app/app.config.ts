2fa0916ce6419b8ea51dbc9163875b89043bb2ef
malka3949 <ms0534113949@gmail.com>
2026-01-14 12:21:34 +0200

    Revert "crud company and tasks"
    This reverts commit 3f08f326665bad4db49a114dbcedbcd8da41a15d.

--- Fronted/Angular-app/src/app/app.config.ts
+++ Fronted/Angular-app/src/app/app.config.ts
@@ -1,4 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+// import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+
+// import { routes } from './app.routes';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideBrowserGlobalErrorListeners(),
+//     provideRouter(routes)
+//   ]
+// };
+
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners,provideZoneChangeDetection  } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient,withInterceptors  } from '@angular/common/http';  // ⬅️ הוסף את זה
 import { provideAnimations } from '@angular/platform-browser/animations';
