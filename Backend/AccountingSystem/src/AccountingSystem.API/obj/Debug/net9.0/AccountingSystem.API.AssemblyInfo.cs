fe35aff3dc1697fae479206b9cd66c7302c61430
malka3949 <ms0534113949@gmail.com>
2026-01-18 12:36:59 +0200

    googleClientId

--- Backend/AccountingSystem/src/AccountingSystem.API/obj/Debug/net9.0/AccountingSystem.API.AssemblyInfo.cs
+++ Backend/AccountingSystem/src/AccountingSystem.API/obj/Debug/net9.0/AccountingSystem.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountingSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b7af42488c505a8fe3dedfe9b585ac692ce0cdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c1eee208a79a1da818a6659dacf54dea91d6b03")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountingSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountingSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Fronted/Angular-app/src/app/app.config.env.example.ts
+++ Fronted/Angular-app/src/app/app.config.env.example.ts
@@ -2,5 +2,5 @@
 // This file is generated from .env - keep them in sync
 export const envConfig = {
   apiUrl: 'https://localhost:7118/api',
-  googleClientId: 'YOUR_GOOGLE_CLIENT_ID_HERE'
+  googleClientId: 'jhjrelhsrdg7j27mhkhbr0vcecvj938u'
 };
